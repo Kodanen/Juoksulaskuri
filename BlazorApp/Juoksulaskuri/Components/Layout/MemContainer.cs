f236023ee627c90ee5766c9c2e2982c7f7f4f49c
Kodanen <jesse.luhti@gmail.com>
2023-12-31 09:09:14 +0200

    Test deployment

---
+++ BlazorApp/Juoksulaskuri/Components/Layout/MemContainer.cs
@@ -0,0 +1,20 @@
+﻿using Juoksulaskuri.Core;
+
+namespace Juoksulaskuri
+{
+    public class MemContainer
+    {
+        #region UI update
+        public void Update()
+        {
+            NotifyStateChanged();
+        }
+
+        public event Action? OnChange;
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+        #endregion
+
+        public string Name { get; set; }    
+    }
+}
